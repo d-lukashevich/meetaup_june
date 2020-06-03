a141e124682b31085b9a660cfb70264dee785037
Denis Lukashevich <denis.lukashevich@megafon.ru>
2020-06-03 20:41:12 +0300

    make basic table

--- src/components/TablePage/index.js
+++ src/components/TablePage/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Table, Thead, Tbody, Row, Cell } from 'core-lib-react/components/Table';
 
 const Page = ({ people }) => {
   return (
     <div>
-      {people.map(({ name }) => {
-        return <div key={name}>{name}</div>;
-      })}
+      <Table>
+        <Thead>
+          <Row>
+            <Cell>Name</Cell>
+            <Cell>Gender</Cell>
+            <Cell>Homeworld</Cell>
+            <Cell>Eye color</Cell>
+            <Cell>Height</Cell>
+          </Row>
+        </Thead>
+        <Tbody>
+          {people.map(({ name, gender, homeworld, eye_color, height }) => (
+            <Row key={name}>
+              <Cell>{name}</Cell>
+              <Cell>{gender}</Cell>
+              <Cell>{homeworld}</Cell>
+              <Cell>{eye_color}</Cell>
+              <Cell>{height}</Cell>
+            </Row>
+          ))}
+        </Tbody>
+      </Table>
     </div>
   );
 };
