b0c6599dc0bdc85d5c11d4a475f059e1146ddbcb
Denis Lukashevich <denis.lukashevich@megafon.ru>
2020-06-03 17:36:49 +0300

    add CLR and common settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RebootCss from 'core-lib-react/globalStyles/RebootCss';
+import Fonts from 'core-lib-react/globalStyles/Fonts';
+import CommonStyles from 'core-lib-react/globalStyles/CommonStyles';
+
+import TablePage from './components/TablePage';
 
 function App() {
+  fetch('https://swapi.dev/api/people/')
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RebootCss />
+      <Fonts />
+      <CommonStyles />
+      <div style={{ padding: 15 }}>
+        <TablePage />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/TablePage/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = (props) => {
+  return <div {...props}>here will be the table</div>;
+};
+
+export default Page;
